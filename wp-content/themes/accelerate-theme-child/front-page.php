0b15f5eb6695827616f6c7f1a747f02bf82268ce
ivan_l <ivan7538@gmail.com>
2020-12-11 14:01:05 -0800

    front page line item 33

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -30,7 +30,7 @@ get_header(); ?>
 			<ul class="homepage-featured-work">
 			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 				<?php while ( have_posts() ) : the_post();
-					$image_1 = get_field(image_1);
+					$image_1 = get_field("image_1");
 					$size = "medium";
 				?>
 				<li class="individual-featured-work">
